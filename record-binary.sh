d99220e4c958b20bb27b766be4c20227e0c48594
Mike <tankf33der@disroot.org>
2023-03-12 13:18:51 +0200

    .

--- record-binary.sh
+++ record-binary.sh
@@ -10,4 +10,18 @@ cd pijul-tests-data
 pijul init repo
 cd repo
 
+for _ in {0..16}; do
+	pijul archive -o mike
+	pijul add -rf .pijul mike.tar.gz
+	pijul record -am"."
+
+	cd ..
+	pijul clone repo repo2
+	cd repo2
+	gzip -t mike.tar.gz
+	cd ..
+	rm -rf repo2
+	cd repo
+done
+
 echo "OK--record-binary"
