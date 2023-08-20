81fdb47fab623efc641498fff0f034319e78d376
Mike <tankf33der@disroot.org>
2023-08-20 09:10:54 +0300

    .

--- add.sh
+++ add.sh
@@ -10,7 +10,7 @@ cd pijul-tests-data
 pijul init repo
 cd repo
 
-# 2027 files, 121 dirs
+# 2021 files, 121 dirs, 1 .ignore
 tar -xJf ../../pijul-tests/kernel/linux-2.0.1.tar.xz --strip-components=1
 add
 eq 2143 "$(pijul ls | wc -l)"
