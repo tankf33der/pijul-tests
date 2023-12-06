b09e035570b71c95da2700470368daad35594c42
Mike <mpech@tuta.io>
2023-12-06 11:43:33 +0200

    .

---
+++ apply-1.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -x -e
+
+source ./functions.sh
+
+cd ..
+rm -rf pijul-tests-data
+mkdir pijul-tests-data
+cd pijul-tests-data
+pijul init repo
+cd repo
+
+tar -xJf ../../pijul-tests/kernel/linux-2.0.1.tar.xz --strip-components=1
+add
+record
+for i in {2..40}; do
+   xzcat ../../pijul-tests/patches/patch-2.0."$i".xz | patch -sp1
+   add
+   record
+done
+
+pijul channel new mike1
+for H in $(pijul credit Makefile | grep -o "^[[:alnum:]]*" | sort -u | shuf); do
+	pijul apply --channel mike1 "$H"
+done
+
+i=1
+for H in $(pijul log --hash-only | head -9); do
+	pijul channel new "$i"
+	pijul apply "$H" --channel "$i"
+	i=$((i+1))
+done
+
+echo "OK--apply-1"
