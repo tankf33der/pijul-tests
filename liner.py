7855d549e66922717958c273fd458b9d6ef722be
Mike <mpech@tuta.io>
2023-12-18 13:12:56 +0200

    .

--- liner.py
+++ liner.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 import random
+import time
+
+random.seed(int(str(time.time()).split(".")[1]))
 
 with open('peace-and-war.txt', 'r') as f:
     lines = []
--- peace-liner.sh
+++ peace-liner.sh
@@ -10,9 +10,11 @@ cd pijul-tests-data
 pijul init repo
 cd repo
 
+# ready!
 cp ../../pijul-tests/liner.py .
 cp ../../pijul-tests/peace-and-war.txt .
 
+# step 1
 pijul add peace-and-war.txt
 record
 
@@ -20,5 +22,35 @@ for _ in {0..128}; do
 	./liner.py
 	record
 done
+
+# step 2
+cd ..
+pijul clone repo repo2
+cd repo2
+pijul pull -a
+
+cd ..
+pijul clone repo2 repo3
+cd repo3
+pijul pull -a
+
+# step 3, delete file and push
+cd ..
+cd repo3
+pijul remove peace-and-war.txt
+record
+pijul push -a
+
+# final step, push second bulk of records
+cd ..
+cd repo
+for _ in {0..128}; do
+	./liner.py
+	record
+done
+cd ..
+cd repo2
+pijul pull -a
+# the fin
 
 echo "OK--peace-liner"
