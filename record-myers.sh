8959af512def8a2a648fa12c0c837bdf982ff98d
Mike <tankf33der@disroot.org>
2024-01-13 20:30:13 +0200

    .

--- record-myers.sh
+++ record-myers.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -x -e
 
+python='python3'
+u=$(uname)
+if [ "$u" = "NetBSD" ]; then
+	python='python3.12'
+fi
+
 source ./functions.sh
 
 cd ..
@@ -15,19 +21,19 @@ cp ../../pijul-tests/utf8-sum.py .
 cp ../../pijul-tests/utf8-shuffle.py .
 
 # ready!
-./utf8-sum.py utf8.txt
+"$python" ./utf8-sum.py utf8.txt
 add
 record
 
 for _ in {0..128}; do
-	./utf8-shuffle.py utf8.txt
+	"$python" ./utf8-shuffle.py utf8.txt
 	mv utf8-2.txt utf8.txt
 	record
 
 	cd ..
 	pijul clone repo repo2
 	cd repo2
-	./utf8-sum.py utf8.txt
+	"$python" ./utf8-sum.py utf8.txt
 	cd ..
 	rm -rf repo2
 	cd repo
@@ -37,19 +43,19 @@ done
 cd ..
 pijul clone repo repo2
 cd repo2
-./utf8-sum.py utf8.txt
+"$python" ./utf8-sum.py utf8.txt
 
 # fork
 cd ../repo
 pijul fork fork1
 pijul channel switch fork1
-./utf8-sum.py utf8.txt
+"$python" ./utf8-sum.py utf8.txt
 pijul channel switch main
 
 # channel new
 pijul channel new new1
 pijul channel switch new1
-./utf8-sum.py utf8.txt
+"$python" ./utf8-sum.py utf8.txt
 pijul channel switch main
 
 # tag
@@ -57,6 +63,6 @@ pijul tag create -m"."
 LAST="$(pijul tag  | grep State | awk '{print $2}')"
 pijul tag checkout "$LAST"
 pijul channel switch "$LAST"
-./utf8-sum.py utf8.txt
+"$python" ./utf8-sum.py utf8.txt
 
 echo "OK--record-myers"
--- record-patience.sh
+++ record-patience.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -x -e
 
+python='python3'
+u=$(uname)
+if [ "$u" = "NetBSD" ]; then
+	python='python3.12'
+fi
+
 source ./functions.sh
 
 cd ..
@@ -15,19 +21,19 @@ cp ../../pijul-tests/utf8-sum.py .
 cp ../../pijul-tests/utf8-shuffle.py .
 
 # ready!
-./utf8-sum.py utf8.txt
+"$python" ./utf8-sum.py utf8.txt
 add
 pijul record --patience -am"."
 
 for _ in {0..128}; do
-	./utf8-shuffle.py utf8.txt
+	"$python" ./utf8-shuffle.py utf8.txt
 	mv utf8-2.txt utf8.txt
 	pijul record --patience -am"."
 
 	cd ..
 	pijul clone repo repo2
 	cd repo2
-	./utf8-sum.py utf8.txt
+	"$python" ./utf8-sum.py utf8.txt
 	cd ..
 	rm -rf repo2
 	cd repo
@@ -37,6 +43,6 @@ for _ in {0..128}; do
 cd ..
 pijul clone repo repo2
 cd repo2
-./utf8-sum.py utf8.txt
+"$python" ./utf8-sum.py utf8.txt
 
 echo "OK--record-patience"
