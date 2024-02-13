75873c47c8b7fa0bd0f3c1522824c9ec1ab56b6e
Mike <tankf33der@disroot.org>
2024-02-13 09:08:06 +0200

    .

---
+++ partial-all.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -x -e
+
+source ./functions.sh
+
+cd ..
+rm -rf pijul-tests-data
+mkdir pijul-tests-data
+cd pijul-tests-data
+pijul init repo
+cd repo
+
+tar -xJf ../../pijul-tests/kernel/linux-2.0.1.tar.xz --strip-components=1
+add
+record
+
+for i in {2..40}; do
+	xzcat ../../pijul-tests/patches/patch-2.0."$i".xz | patch -sp1
+	add
+	record
+done
+
+echo "OK--partial-all"
