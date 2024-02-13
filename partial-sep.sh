ce8e8e63b30a136a2bc6d21a822806882fb1d438
Mike <tankf33der@disroot.org>
2024-02-13 21:23:05 +0200

    .

---
+++ partial-sep.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -x -e
+
+source ./functions.sh
+
+cd ..
+rm -rf pijul-tests-data
+mkdir pijul-tests-data
+cd pijul-tests-data
+pijul init repo
+cd repo
+
+echo "OK--partial-sep"
