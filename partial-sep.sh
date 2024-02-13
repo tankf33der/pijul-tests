6982bbfa3bcba0cb742383cee2f53167b591d7aa
Mike <tankf33der@disroot.org>
2024-02-13 21:46:49 +0200

    .

--- partial-sep.sh
+++ partial-sep.sh
@@ -10,4 +10,28 @@ cd pijul-tests-data
 pijul init repo
 cd repo
 
+mkdir a
+cd a
+for _ in {0..15}; do
+   touch "$(mktemp mike-XXXX)"
+   add
+   record
+done
+cd ..
+
+mkdir b
+cd b
+for _ in {0..15}; do
+   touch "$(mktemp mike-XXXX)"
+   add
+   record
+done
+cd ..
+
+cd ..
+pijul clone --path a repo a
+pijul clone --path b repo b
+pijul log --hash-only --repository a > a.records
+pijul log --hash-only --repository b > b.records
+
 echo "OK--partial-sep"
