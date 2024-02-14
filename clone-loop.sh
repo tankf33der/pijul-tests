2c0edd04c76971b09774ca86f65c814bde676a6e
Mike <tankf33der@disroot.org>
2024-02-14 08:52:12 +0200

    .

--- clone-loop.sh
+++ clone-loop.sh
@@ -1,12 +1,29 @@
 #!/usr/bin/env bash
 set -x -e
 
-cd ..
-rm -rf pjtemp
 
-for _ in {0..10}; do
-	pijul clone tankf33der@ssh.pijul.com:pijul/pijul pjtemp
-	rm -rf pjtemp
+#!/usr/bin/env bash
+set -x -e
+
+source ./functions.sh
+
+cd ..
+rm -rf pijul-tests-data
+mkdir pijul-tests-data
+cd pijul-tests-data
+pijul init repo
+cd repo
+
+mkdir a
+cd a
+for _ in {0..15}; do
+   touch "$(mktemp mike-XXXX)"
+   add
+   record
 done
+cd ..
+cd ..
+
+pijul clone --path a repo a
 
 echo "OK--clone-loop"
