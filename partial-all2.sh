a111721e0b94033fec82d6091a5451dc8ed035fa
Mike <tankf33der@disroot.org>
2024-02-15 20:55:48 +0200

    .

---
+++ partial-all2.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+set -x -e
+
+source ./functions.sh
+
+cd ..
+rm -rf pijul-tests-data
+mkdir pijul-tests-data
+cd pijul-tests-data
+pijul init repo
+cd repo
+
+mkdir a
+cd a
+tar -xJf ../../../pijul-tests/kernel/linux-2.0.1.tar.xz --strip-components=1
+add
+record
+
+for i in {2..40}; do
+	xzcat ../../../pijul-tests/patches/patch-2.0."$i".xz | patch -sp1
+	add
+	record
+done
+
+cd ..
+mkdir b
+cd b
+tar -xJf ../../../pijul-tests/kernel/linux-2.0.1.tar.xz --strip-components=1
+add
+record
+
+for i in {2..40}; do
+	xzcat ../../../pijul-tests/patches/patch-2.0."$i".xz | patch -sp1
+	add
+	record
+done
+cd ..
+cd ..
+
+pijul clone --path a repo a
+pijul clone --path b repo b
+pijul log --hash-only --repository a > a.records
+pijul log --hash-only --repository b > b.records
+eq 1 "$(comm -12 <(sort a.records) <(sort b.records) | wc -l)"
+
+echo "OK--partial-all2"
