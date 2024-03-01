25f2bc7b61c91ab114565e98e15c1e9d10e39794
Mike <tankf33der@disroot.org>
2024-03-01 19:35:25 +0200

    .

---
+++ push-loop.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -x -e
+
+source ./functions.sh
+
+cd ..
+rm -rf pijul-tests-data
+mkdir pijul-tests-data
+cd pijul-tests-data
+pijul init p1
+pijul clone p1 p2
+cd p2
+
+touch a
+pijul add a
+pijul rec -am.
+
+touch b
+pijul add b
+pijul rec -am.
+
+pijul push --path a -a
