f2a932a10d6bb84e0f43e2d8aa0be41624877ea4
Mike <tankf33der@disroot.org>
2024-04-10 04:14:28 +0300

    .

--- b-pushmeta.sh
+++ b-pushmeta.sh
@@ -2,6 +2,8 @@
 
 set -x -e
 
+dd='dd if=/dev/urandom of=A.dat bs=1024 count=512'
+
 source ./functions.sh
 
 cd ..
@@ -10,8 +12,22 @@ mkdir pijul-tests-data
 cd pijul-tests-data
 pijul init repo
 pijul clone repo repo2
-cp ../pijul-tests/B.txt repo2/A.txt
-cp ../pijul-tests/assert.py repo2/
 cd repo2
+
+touch A.dat
+cksum A.dat | awk '{print $1}' > A.meta
+add
+record
+for _ in {0..128}; do
+pijul push -a
+cd ../repo
+
+name=$(cat A.meta)
+str "$name" "$(cksum A.dat | awk '{print $1}')"
+cd ../repo2
+eval "$dd"
+cksum A.dat | awk '{print $1}' > A.meta
+record
+done
 
 echo "OK--b-pushmeta"
