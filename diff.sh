d8611e00ea1b4beae0ba602449a4b0d8fe6352f8
Mike <mpech@tuta.io>
2024-04-11 15:41:52 +0300

    .

--- diff.sh
+++ diff.sh
@@ -14,13 +14,13 @@ cp ../../pijul-tests/diff.dat .
 add
 record
 
-# # milestone #1 - text file
-# for i in {0..1054}; do
-# 	echo -ne '.' | dd of=diff.txt bs=1 seek="$i" count=1 conv=notrunc
-# 	eq 1 "$(pijul diff -s | wc -l)"
-# 	eq 1 "$(pijul diff -s --patience | wc -l)"
-# 	pijul reset --force
-# done
+# milestone #1 - text file
+for i in {0..1054}; do
+	echo -ne '.' | dd of=diff.txt bs=1 seek="$i" count=1 conv=notrunc
+	eq 1 "$(pijul diff -s | wc -l)"
+	eq 1 "$(pijul diff -s --patience | wc -l)"
+	pijul reset --force
+done
 
 # milestone #2 -
 for i in {0..1023}; do
