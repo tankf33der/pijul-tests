826b48078a9961ed24edf60aab7861b6cb67fbe0
Mike <mpech@tuta.io>
2024-04-26 17:07:48 +0300

    .

---
+++ stairs.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+set -x -e
+
+source ./functions.sh
+
+cd ..
+rm -rf pijul-tests-data
+mkdir pijul-tests-data
+cd pijul-tests-data
+pijul init repo
+cd repo
+touch a
+add
+record
+
+cd ..
+pijul clone repo repo2
+cd repo2
+
+# milestone #1: stairs UP
+for i in {1..4096}; do
+	dd if=/dev/random of=a seek=0 count=1 bs="$i" conv=notrunc
+	record
+done
+
+# milestone #2: stairs DOWN
+for i in {4096..1}; do
+	dd if=/dev/random of=a seek=0 count=1 bs="$i"
+	record
+done
+
+pijul push -a
+cd ../repo
+eq 1 "$(cksum a | awk '{print $2}')"
+cd ..
+pijul clone repo repo3
+
+echo "OK--diff"
