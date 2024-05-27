f0c14fbab6d203a80634136f84ddc800c3663b38
Mike <mpech@tuta.io>
2024-05-27 16:01:29 +0300

    .

--- add-viafiles.sh
+++ add-viafiles.sh
@@ -10,12 +10,17 @@ cd pijul-tests-data
 pijul init repo
 cd repo
 
+pijul channel new mike
 tar -xJf ../../pijul-tests/kernel/linux-2.0.1.tar.xz --strip-components=1
 find kernel/ -type f -exec sh -c " pijul add "{}" && pijul rec -am"."" \;
+pijul apply --channel mike $(pijul log --hash-only | shuf)
 find net/ -type f -exec sh -c " pijul add "{}" && pijul rec -am"."" \;
+pijul apply --channel mike $(pijul log --hash-only | shuf)
 find drivers/ -type f -exec sh -c " pijul add "{}" && pijul rec -am"."" \;
+pijul apply --channel mike $(pijul log --hash-only | shuf)
 pijul add Makefile
 record
+pijul apply --channel mike $(pijul log --hash-only | shuf)
 
 for i in {2..40}; do
 	pijul fork "$i"
@@ -30,11 +35,16 @@ for i in {2..40}; do
 	pijul channel switch main
 done
 
+pijul apply --channel mike $(pijul log --hash-only | shuf)
+
 pijul channel switch 28
 H=$(pijul log --hash-only | head -1)
 pijul apply "$H" --channel main
 pijul channel switch main
-
 crc Makefile 2090994418
+
+pijul channel switch mike
+crc Makefile 2090994418
+pijul channel switch main
 
 echo "OK--add-viafiles"
