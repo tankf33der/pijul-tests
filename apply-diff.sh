f5b96eeb920aeb02361ad1a1e435a0bd79a2c4f2
Mike <tankf33der@disroot.org>
2024-06-01 22:10:56 +0300

    .

--- apply-diff.sh
+++ apply-diff.sh
@@ -19,6 +19,12 @@ for i in {1..335}; do
    xzcat ../../pijul-tests/patches/patch-4.14."$i"-*.xz | patch -Esp1
    add
    record
+
+   H="$(pijul log --hash-only | head -1)"
+   pijul apply "${H}" --channel m
+   pijul channel switch m
+   pijul channel switch main
+   zero $(pijul diff --channel m | wc -l)
 done
 
 echo "OK--apply-diff"
