2dcb65a01bb86330501b90877bfd0680655d6369
Mike <tankf33der@disroot.org>
2024-06-01 22:03:48 +0300

    .

--- patches/wget.sh
+++ patches/wget.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -x -e
 
-for i in {2..3}; do
-    wget https://cdn.kernel.org/pub/linux/kernel/v4.x/patch-4.14.${i}.tar.xz
+for i in {1..335}; do
+    wget https://cdn.kernel.org/pub/linux/kernel/v4.x/incr/patch-4.14.${i}-$((i+1)).xz
 done
+
