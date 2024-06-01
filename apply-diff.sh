2e73180ab112da9816ba123bfafcbeb7e3ace6ea
Mike <tankf33der@disroot.org>
2024-06-01 22:03:59 +0300

    .

---
+++ apply-diff.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -x -e
+
+source ./functions.sh
+
+cd ..
+rm -rf pijul-tests-data
+mkdir pijul-tests-data
+cd pijul-tests-data
+pijul init repo
+cd repo
+pijul fork m
+
+tar -xJf ../../pijul-tests/kernel/linux-4.14.1.tar.xz --strip-components=1
+add
+record
+
+for i in {1..335}; do
+   xzcat ../../pijul-tests/patches/patch-4.14."$i"-*.xz | patch -Esp1
+   add
+   record
+done
+
+echo "OK--apply-diff"
